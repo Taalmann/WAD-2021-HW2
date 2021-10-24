c51293f13b532c8f76a55e6a5e50f7f7a0807e6a
RitumsC <ritums.cepitis@ut.ee>
2021-10-24 17:00:06 +0200

    Add cleaner code for posts from json and add explanation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # WAD-2021-HW2
 Homework 2
+
+
+## Description for work on Tasks 4 and 5
+We define javascript function in file "posts.js" and put that in resources folder.
+We call this function in index.html file when we want to display posts. 
+
+The main logic for the function is to first determine json source.
+
+In task we do this by defining:
+
+    jsonSource = "https://api.npoint.io/d6d2f9f973bc3db141eb"
+
+and then in function collecting from this source using jquery function 
+
+    $.getJSON(jsonSource)
+
+Once we have retrieved from json source we go through each post in json and construct appropriate post html code and append that to post section using jquery.
+
+First of all, we do parsing of json file to determine wheter user has defined avatar, and wheter the post contains image (this should change the style of post sligthly).
+
+After that is done, we sum up individual html parts and use jquery appendTo function.
+
+    $( "posts html code here" ).appendTo("#post-body")
+
+For Task 5 we define the json file localy and hence we have to change the jsonSource accordingly to, with rest remaining the same
+
+    // jsonSource = "https://api.npoint.io/d6d2f9f973bc3db141eb"
+    jsonSource = "/resources/json/posts.json"
--- resources/js/posts.js
+++ resources/js/posts.js
@@ -1,58 +1,49 @@
 
-// Load JSON from endpoint
-$(function(){
-    $.getJSON("https://api.npoint.io/d6d2f9f973bc3db141eb").done(
-        function( json ) {
-            for (post in json){
-                console.log(json[post].none)
+// Load posts using from hardcoded json source
+function loadPosts() {$(function(){
+  // jsonSource = "https://api.npoint.io/d6d2f9f973bc3db141eb"
+  jsonSource = "/resources/json/posts.json"
 
-                // Select user avatar from user's ID
-                if (json[post].user === 0) {
-                    var avatar = "resources/img/default_avatar.png"
-                }
-                else {
-                    var avatar = "resources/img/avatar_user_"+json[post].user+".jpg"
-                }
+  $.getJSON(jsonSource).done(
+    function( json ) {
+      for (post in json){
+        // Select user avatar from user's ID or fallback to default
+        if (json[post].user) {
+          var avatar = "resources/img/avatar_user_"+json[post].user+".jpg"
+        } else {
+          var avatar = "resources/img/default_avatar.png"
+        }
 
-                // Chech if post has image
-                var hasPhoto
-                json[post].photo ? hasPhoto = 1 :hasPhoto = 0
-                console.log(json[post].photo)
+        // Chech if post has image
+        var photoDiv = ''
+        if (json[post].photo) {
+          photoDiv = '<div class="post_photo">' +
+            '<img src="' + json[post].photo + '" class="photo" alt="Kassari sääretirp">' +
+          '</div>'
+        } 
 
-                if (hasPhoto){
-                    $(
-                        '<div class="post">' +
-                            '<div class="post_header">' +
-                            '<img src='+avatar+' class="avatar" alt="User avatar"></img>' +
-                            '<p>'+json[post].date+'</p>' +
-                            '</div>' +
-                            '<div class="post_photo">' +
-                            '<img src="' + json[post].photo + '" class="photo" alt="Kassari sääretirp">' +
-                            '</div>' +
-                            '<p>'+
-                            json[post].text +
-                            '</p>' +
-                            '<div class="post_footer">' +
-                            '<img src="resources/img/thumb_up.png" class="thumb_up" alt="Thumb up">' +
-                            '</div>' +
-                        '</div>'
-                    ).appendTo("#post-body")
-                } else {
-                    $(
-                        '<div class="post">' +
-                            '<div class="post_header">' +
-                            '<img src='+avatar+' class="avatar" alt="User avatar"></img>' +
-                            '<p>'+json[post].date+'</p>' +
-                            '</div>' +
-                            '<p>'+
-                            json[post].text +
-                            '</p>' +
-                            '<div class="post_footer">' +
-                            '<img src="resources/img/thumb_up.png" class="thumb_up" alt="Thumb up">' +
-                            '</div>' +
-                        '</div>'
-                    ).appendTo("#post-body")
-                }
-            }
-        })
-})
+        // Construct post's HTML and add to #post-body section
+        $(
+          '<div class="post">' +
+            // Post header, avatar + timestamp
+            '<div class="post_header">' +
+              '<img src='+avatar+' class="avatar" alt="User avatar"></img>' +
+              '<p>'+json[post].date+'</p>' +
+            '</div>' +
+            // Photo field if exists
+            photoDiv +
+            // Post text content
+            '<p>'+
+              json[post].text +
+            '</p>' +
+            // Post footer
+            '<div class="post_footer">' +
+              '<img src="resources/img/thumb_up.png" class="thumb_up" alt="Thumb up">' +
+            '</div>' +
+          '</div>'
+        ).appendTo("#post-body")
+      }
+    }
+  )
+  })
+}
